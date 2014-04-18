05e870022ec7f09430c4d646a4c99eb9a8bfa632
bcoe <bencoe@gmail.com>
2014-04-18 15:50:27 -0700

    removed unused tests folder.

--- tests/atomic-json-test.js
+++
@@ -1,8 +0,0 @@
-var Lab = require('lab'),
-  redisClient = require('redis').createClient,
-  atomicJSON = require('../lib');
-
-Lab.test('returns true when 1 + 1 equals 2', function (done) {
-    Lab.expect(1+1).to.equal(2);
-    done();
-});
